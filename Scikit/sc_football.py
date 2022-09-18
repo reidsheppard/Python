06044ad3cd3ecae805dd8d11053cf548028be6c0
Gavin <sportygavin@gmail.com>
2022-09-18 16:36:59 -0400

    Built sklearn program

--- Scikit/sc_football.py
+++ Scikit/sc_football.py
@@ -1,3 +1,5 @@
+from array import array
+from cgi import test
 from lightgbm import LGBMRegressor
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,8 +10,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression, RidgeCV
 from sklearn.svm import LinearSVR
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import KNNImputer
+from sklearn import preprocessing
 
-df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRaYPRkDlXo4_PvOwE55UqyO1oEQP4spBXEPy34mtTixFdgEDmxne0LleWT8hzgqqGDdoi75LWP0DVP/pub?gid=1767393397&single=true&output=csv', index_col=0)
+# imputer = KNNImputer(n_neighbors= 4, weights = 'uniform')
+encoder = preprocessing.LabelEncoder()
 
-print(df.shape)
-df.head()
+trainingData = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRaYPRkDlXo4_PvOwE55UqyO1oEQP4spBXEPy34mtTixFdgEDmxne0LleWT8hzgqqGDdoi75LWP0DVP/pub?gid=1767393397&single=true&output=csv', index_col=0)
+testData = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRaYPRkDlXo4_PvOwE55UqyO1oEQP4spBXEPy34mtTixFdgEDmxne0LleWT8hzgqqGDdoi75LWP0DVP/pub?gid=1489051496&single=true&output=csv', index_col=0 )
+
+encoder.fit(trainingData)
+transformed_testData = encoder.transform(testData)
+
+print(testData)
+
+print("transformed_testData")
+print(transformed_testData)
+
+
