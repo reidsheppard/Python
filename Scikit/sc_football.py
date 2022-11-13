f0227eb267e5c9a80177c7660095c28db874cd32
Gavin <sportygavin@gmail.com>
2022-11-13 16:46:03 -0500

    ChangedSciHub code

--- Scikit/sc_football.py
+++ Scikit/sc_football.py
@@ -1,31 +1,54 @@
 from array import array
 from cgi import test
+import sklearn
 from lightgbm import LGBMRegressor
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import sklearn
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.linear_model import LinearRegression, RidgeCV
 from sklearn.svm import LinearSVR
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import KNNImputer
 from sklearn import preprocessing
-
-# imputer = KNNImputer(n_neighbors= 4, weights = 'uniform')
-encoder = preprocessing.LabelEncoder()
-
-trainingData = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRaYPRkDlXo4_PvOwE55UqyO1oEQP4spBXEPy34mtTixFdgEDmxne0LleWT8hzgqqGDdoi75LWP0DVP/pub?gid=1767393397&single=true&output=csv', index_col=0)
-testData = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRaYPRkDlXo4_PvOwE55UqyO1oEQP4spBXEPy34mtTixFdgEDmxne0LleWT8hzgqqGDdoi75LWP0DVP/pub?gid=1489051496&single=true&output=csv', index_col=0 )
-
-encoder.fit(trainingData)
-transformed_testData = encoder.transform(testData)
-
-print(testData)
-
-print("transformed_testData")
-print(transformed_testData)
-
-
+from sklearn.ensemble import RandomForestRegressor
+ 
+ 
+trainingData = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRaYPRkDlXo4_PvOwE55UqyO1oEQP4spBXEPy34mtTixFdgEDmxne0LleWT8hzgqqGDdoi75LWP0DVP/pub?gid=1879927646&single=true&output=csv', index_col=0)
+testData = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRaYPRkDlXo4_PvOwE55UqyO1oEQP4spBXEPy34mtTixFdgEDmxne0LleWT8hzgqqGDdoi75LWP0DVP/pub?gid=1056049758&single=true&output=csv', index_col=0)
+ 
+# What it uses to fill in missing values
+imputer = KNNImputer(n_neighbors=4)
+ 
+label = 'Yds'
+y_train = trainingData[label] # values to predict
+x_train = trainingData.drop(columns=[label]) # features
+ 
+y_test = testData[label]
+x_test = testData.drop(columns=[label]) # features
+ 
+ 
+model = LinearRegression()
+model.fit(x_train, y_train)
+ 
+predictions = model.predict(x_test)
+ 
+ 
+i = 1
+print("Game      Yds predicted")
+for x in predictions:
+   print(i, "\t",x)
+   i+=1
+ 
+ 
+print('mean_squared_error : ', mean_squared_error(y_test, predictions))
+print('mean_absolute_error : ', mean_absolute_error(y_test, predictions))
+ 
+ 
+ 
+# training the data
+# imputer.fit(trainingData)
+# transformed_testData = imputer.transform(testData)
+ 
